cedc7aae931ce0f3b353df6972cfa8722094ab16
training_d5.01.02 <training_d5.01.02@DDC5-D-4VM5T72.dir.svc.accenture.com>
2018-07-18 11:54:55 +0530

    COMMIT 4

---
+++ log4j/src/log4j/sample2.java
@@ -0,0 +1,12 @@
+package log4j;
+
+public class sample2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Checking code commint in GIT");
+
+	}
+
+}
